9e7c62df24e99717a9d9aa5edff3ab537de8388d
ykyki <bsyki@pm.me>
2023-04-19 23:42:50 +0900

    update hello/main.rs

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    hello(&mut std::io::stdout())?;
+    Ok(())
+}
+
+fn hello(writer: &mut impl std::io::Write) -> std::io::Result<()> {
+    writeln!(writer, "Hello, world!")?;
+    writeln!(writer, "This is ykyki.")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hello() -> std::io::Result<()> {
+        let mut buf = Vec::new();
+
+        hello(&mut buf)?;
+
+        assert_eq!(buf, b"Hello, world!\nThis is ykyki.\n");
+        Ok(())
+    }
 }
