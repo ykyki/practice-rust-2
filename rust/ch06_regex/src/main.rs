3ef3ba88b38a0e214711d69c423cccd0d981ec92
ykyki <bsyki@pm.me>
2023-04-21 21:56:39 +0900

    ch06: add main format

--- rust/ch06_regex/src/main.rs
+++ rust/ch06_regex/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use crate::helper::DynError;
+
+mod engine;
+mod helper;
+
+fn main() -> Result<(), DynError> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("usage: {} regex file", args[0]);
+        return Err("invalid arguments".into());
+    }
+
+    match_file(&args[1], &args[2])?;
+
+    Ok(())
+}
+
+fn match_file(_expr: &str, _file: &str) -> Result<(), DynError> {
+    todo!()
 }
