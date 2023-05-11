cc616e58edf3b5c5d50d026564bf6e81cf173927
ykyki <bsyki@pm.me>
2023-05-11 21:59:27 +0900

    ch06: add benchmark

---
+++ rust/ch06_regex/benches/benchmark.rs
@@ -0,0 +1,64 @@
+use ch06_regex::do_matching;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::time::Duration;
+
+/// (計測のid、a?^n a^nという正規表現、文字列)というタプル
+const INPUTS: &[(&str, &str, &str)] = &[
+    ("n = 2", "a?a?aa", "aa"),
+    ("n = 4", "a?a?a?a?aaaa", "aaaa"),
+    ("n = 6", "a?a?a?a?a?a?aaaaaa", "aaaaaa"),
+    ("n = 8", "a?a?a?a?a?a?a?a?aaaaaaaa", "aaaaaaaa"),
+    ("n = 10", "a?a?a?a?a?a?a?a?a?a?aaaaaaaaaa", "aaaaaaaaaa"),
+    (
+        "n = 12",
+        "a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaa",
+        "aaaaaaaaaaaa",
+    ),
+    (
+        "n = 14",
+        "a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaa",
+        "aaaaaaaaaaaaaa",
+    ),
+    (
+        "n = 16",
+        "a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaa",
+        "aaaaaaaaaaaaaaaa",
+    ),
+    (
+        "n = 18",
+        "a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaa",
+        "aaaaaaaaaaaaaaaaaa",
+    ),
+    (
+        "n = 20",
+        "a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaa",
+        "aaaaaaaaaaaaaaaaaaaa",
+    ),
+];
+
+fn depth_first(c: &mut Criterion) {
+    let mut g = c.benchmark_group("Depth First");
+    g.measurement_time(Duration::from_secs(12));
+
+    for i in INPUTS {
+        g.bench_with_input(i.0, &(i.1, i.2), |b, args| {
+            b.iter(|| do_matching(args.0, args.1, true))
+        });
+    }
+}
+
+// TODO
+fn width_first(c: &mut Criterion) {
+    let mut g = c.benchmark_group("Width First");
+    g.measurement_time(Duration::from_secs(12));
+
+    for i in INPUTS {
+        g.bench_with_input(i.0, &(i.1, i.2), |b, args| {
+            b.iter(|| do_matching(args.0, args.1, false))
+        });
+    }
+}
+
+criterion_group!(benches, depth_first);
+// criterion_group!(benches, width_first, depth_first); // TODO
+criterion_main!(benches);
